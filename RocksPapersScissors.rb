431248203b32b3f43f63910f0ea2dfe49a92f958
Chris Manna <nitro7777@users.noreply.github.com>
2016-01-01 18:57:39 -0500

    Create RocksPapersScissors.rb

---
+++ RocksPapersScissors.rb
@@ -0,0 +1,15 @@
+class Display
+	ARR = [:ROCKS, :PAPERS, :SCISSORS]
+	
+	def initialize(first_entry,sec_entry = rand(2).round)
+		@entry = entry.upcase.to_sym
+		@sec_entry = sec_entry
+	end
+
+	def display_winner
+		
+	end
+	def entry
+		
+	end
+end
