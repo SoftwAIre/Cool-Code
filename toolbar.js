88c8e784dceadda76938a08ea9400090aa00ee35
Chris Manna <nitro7777@users.noreply.github.com>
2016-05-16 07:41:12 -0400

    Create toolbar.js

---
+++ toolbar.js
@@ -0,0 +1,24 @@
+{        
+        title: 'Work Stuff',
+        iconCls: 'fa-users',
+        items:[{
+            xtype: 'toolbar',
+            layout: {
+                type: 'vbox',
+                align: 'middle'
+            },
+            items: [
+                {
+                    text: 'search', margin:10, style:{ backgroundColor:'blue', borderRadius: '10px' }
+                },
+                {
+                    text: 'Last Result Set', margin:10, style:{backgroundColor:'blue', borderRadius: '10px', contentPadding:40 }
+                },
+                {
+                    text: 'Order Blotter', margin:10, style: { backgroundColor: 'blue', borderRadius: '10px', fontColor: 'white'}
+                },
+                {
+                    text: 'Exit', margin:10, style: { backgroundColor: 'blue', borderRadius: '10px', padding: }
+                }
+           ]
+        }
